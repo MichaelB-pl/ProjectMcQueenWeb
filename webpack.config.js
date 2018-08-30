f9e69a3e24ff0627e0c59f85c3e9105540245d17
Michał Bachta <michal.bachta@outlook.com>
2018-08-30 09:41:37 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: ['./src/index.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'My first test project',
+            template: './src/index.html'
+        })
+    ]
+};
